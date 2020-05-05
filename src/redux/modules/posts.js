4f3e317dbd351e35e496db485a36218fb3721b90
Frank Sun <china-spf@live.com>
2020-05-05 17:17:57 +0800

    #1 Can get and display basic info about post list

---
+++ src/redux/modules/posts.js
@@ -0,0 +1,106 @@
+import { get, put, post } from '../../utils/request';
+import url from '../../utils/url';
+import { actions as appActions } from './app';
+import { combineReducers } from 'redux';
+
+const initialState = {
+    byId: {},
+    allIds: []
+};
+
+// action types
+export const types = {
+    FETCH_ALL_POSTS: "POSTS/FETCH_ALL_POSTS"
+};
+
+// action creators
+export const actions = {
+    // get post list
+    fetchAllPosts: () => {
+        return (dispatch, getState) => {
+            if (shouldFetchAllPosts(getState())) {
+                dispatch(appActions.startRequest());
+                return get(url.getPostList()).then(data => {
+                    dispatch(appActions.finishRequest());
+                    if (!data.error) {
+                        const { posts, postsIds, authors } = convertPostsToPlain(data);
+                        dispatch(fetchAllPostsSuccess(posts, postsIds, authors));
+                    }
+                    else {
+                        dispatch(appActions.setError(data.error));
+                    }
+                });
+            }
+        }
+    }
+};
+
+// action creators - only for internal use, no need to export, so not put them into the "actions" object
+const fetchAllPostsSuccess = (posts, postIds, authors) => ({
+    type: types.FETCH_ALL_POSTS,
+    posts: posts,
+    postIds: postIds,
+    users: authors
+});
+
+const convertPostsToPlain = posts => {
+    let postsById = {};
+    let postsIds = [];
+    let authorsById = {};
+
+    posts.forEach(item => {
+        postsById[item.id] = { ...item, author: item.author.id };
+        postsIds.push(item.id);
+        if (!authorsById[item.author.id]) {
+            authorsById[item.author.id] = item.author;
+        }
+    });
+
+    return {
+        posts: postsById,
+        postsIds: postsIds,
+        authors: authorsById
+    };
+}
+
+// P175， redux的缓存作用. 不是每次re-render都要获取数据. 至于刷新操作，可以加另外一个state field来辨别
+const shouldFetchAllPosts = state => {
+    return !state.posts.allIds || state.posts.allIds.length === 0;
+};
+
+// reducers
+const allIds = (state = initialState.allIds, action) => {
+    switch (action.type) {
+        case types.FETCH_ALL_POSTS:
+            return action.postIds;
+        default:
+            return state;
+    }
+};
+
+const byId = (state = initialState.byId, action) => {
+    switch(action.type){
+        case types.FETCH_ALL_POSTS:
+            return action.posts;
+        default:
+            return state;
+    }
+}
+
+const reducer = combineReducers({
+    allIds,
+    byId
+});
+
+export default reducer;
+
+// selectors
+export const getPostIds = state => {
+    return state.posts.allIds;
+}
+
+export const getPostById = (state, id) => {
+    return state.posts.byId[id];
+};
+
+export const getPostList = state => state.posts.byId;
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,16 @@
+const postListFilter = {
+    fields: ["id", "title", "author", "vote", "updatedAt"],
+    limit: 10,
+    order: "updatedAt DESC",
+    include: "authorPointer",
+    includefilter: {user: {fields: ["id", "username"]}}
+};
+
+function encodeFilter(filter) {
+    return encodeURIComponent(JSON.stringify(filter));
+}
+
 export default {
     login: () => { return "user/login"; },
-    
+    getPostList: () => `/Post?filter=${encodeFilter(postListFilter)}`
 }
